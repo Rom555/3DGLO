e7a721e3654b16e2fefb2b6a45e536d39f9937db
Rom555 <romanzubrilin@gmail.com>
2022-02-23 20:05:24 +0300

    complete lesson20 hard

---
+++ src/modules/scroll.js
@@ -0,0 +1,20 @@
+const scroll = () => {
+  const menuLinks = document.querySelectorAll('menu>ul>li>a');
+  const mainLink = document.querySelector('main>a');
+  const links = [...menuLinks, mainLink];
+
+  links.forEach((link) => {
+    const target = link.attributes.href.value;
+
+    link.addEventListener('click', () => {
+      event.preventDefault();
+
+      document.querySelector(target).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  });
+};
+
+export default scroll;
