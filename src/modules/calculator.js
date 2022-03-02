480040acb01c2aac3aebc4c5262741b8d7fdc400
Rom555 <romanzubrilin@gmail.com>
2022-03-02 22:35:31 +0300

    add calculator

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,11 +1,52 @@
-const calculator = () => {
-  const calc = document.querySelector('.calc-block');
-  const inputsNumber = calc.querySelectorAll('input.calc-item');
+const calculator = (price) => {
+  const calcBlock = document.querySelector('.calc-block');
+  const calcType = calcBlock.querySelector('.calc-type');
+  const calcSquare = calcBlock.querySelector('.calc-square');
+  const calcCount = calcBlock.querySelector('.calc-count');
+  const calcDay = calcBlock.querySelector('.calc-day');
+  const total = calcBlock.querySelector('#total');
 
-  inputsNumber.forEach((input) => {
-    input.addEventListener('input', (e) => {
+  const countCalc = () => {
+    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+    const calcSquareValue = +calcSquare.value;
+
+    let totalValue = 0;
+    let calcCountValue = 1;
+    let calcDayValue = 1;
+
+    if (!calcTypeValue || !calcSquareValue) {
+      total.textContent = 0;
+      return;
+    }
+
+    if (calcCount.value > 1) {
+      calcCountValue += +calcCount.value / 10;
+    }
+
+    if (calcDay.value && calcDay.value < 5) {
+      calcDayValue = 2;
+    } else if (calcDay.value && calcDay.value < 10) {
+      calcDayValue = 1.5;
+    }
+
+    totalValue =
+      price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+
+    total.textContent = totalValue;
+  };
+
+  calcBlock.addEventListener('input', (e) => {
+    if (e.target === calcType) {
+      countCalc();
+    }
+    if (
+      e.target === calcSquare ||
+      e.target === calcCount ||
+      e.target === calcDay
+    ) {
       e.target.value = e.target.value.replace(/\D+/, '');
-    });
+      countCalc();
+    }
   });
 };
 
