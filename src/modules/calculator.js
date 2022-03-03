f2e219da98d837861b1fbb3f1054d76c6a868b3f
Rom555 <romanzubrilin@gmail.com>
2022-03-03 20:51:34 +0300

    add helper.js, animate, change modal and calculator animations

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,3 +1,5 @@
+import { animate } from './helper';
+
 const calculator = (price) => {
   const calcBlock = document.querySelector('.calc-block');
   const calcType = calcBlock.querySelector('.calc-type');
@@ -6,49 +8,43 @@ const calculator = (price) => {
   const calcDay = calcBlock.querySelector('.calc-day');
   const total = calcBlock.querySelector('#total');
 
-  let interval;
-  let count;
-
-  const changetTotal = (num) => {
-    count = 0;
-    let step = num / (price / 10);
-
-    interval = setInterval(() => {
-      total.textContent = count;
-      count += step;
-      if (count > num) clearInterval(interval);
-    }, 30);
-  };
-
   const countCalc = () => {
     const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
     const calcSquareValue = +calcSquare.value;
+    const totalLastValue = +total.textContent;
 
     let totalValue = 0;
     let calcCountValue = 1;
     let calcDayValue = 1;
 
-    if (interval) clearInterval(interval);
-
     if (!calcTypeValue || !calcSquareValue) {
-      total.textContent = 0;
-      return;
+      totalValue = 0;
+    } else {
+      if (calcCount.value > 1) {
+        calcCountValue += +calcCount.value / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        calcDayValue = 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        calcDayValue = 1.5;
+      }
+
+      totalValue =
+        price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
     }
 
-    if (calcCount.value > 1) {
-      calcCountValue += +calcCount.value / 10;
-    }
-
-    if (calcDay.value && calcDay.value < 5) {
-      calcDayValue = 2;
-    } else if (calcDay.value && calcDay.value < 10) {
-      calcDayValue = 1.5;
-    }
-
-    totalValue =
-      price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
-
-    changetTotal(totalValue);
+    animate({
+      duration: 200,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        total.textContent = Math.floor(
+          totalLastValue + (totalValue - totalLastValue) * progress
+        );
+      },
+    });
   };
 
   calcBlock.addEventListener('input', (e) => {
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,22 +1,31 @@
+import { animate } from './helper.js';
+
 const modal = () => {
   const buttons = document.querySelectorAll('.popup-btn');
   const modal = document.querySelector('.popup');
   const modalContent = modal.querySelector('.popup-content');
+  const modalForm = modal.querySelector('form');
 
   const modalContentAnimation = () => {
-    let count = -10;
+    let count = 0;
     let animationId;
+
+    modal.style.width = '0';
     modalContent.style.top = '0';
+    modalContent.style.opacity = '0';
 
     const moveModalContent = () => {
       count++;
 
       animationId = requestAnimationFrame(moveModalContent);
 
-      if (count > 10) {
+      if (count > 20) {
         cancelAnimationFrame(animationId);
       } else {
-        modalContent.style.top = count + '%';
+        modal.style.width = count * 5 + '%';
+        modalContent.style.top = count - 10 + '%';
+        modalContent.style.opacity = count * 0.05;
+        modalForm.style.top = 500 - count * 25 + 'px';
       }
     };
 
@@ -36,9 +45,23 @@ const modal = () => {
     btn.addEventListener('click', () => {
       modal.style.display = 'block';
 
-      if (window.screen.width >= 768) modalContentAnimation();
+      if (window.screen.width >= 768)
+        animate({
+          duration: 500,
+          timing(timeFraction) {
+            return timeFraction;
+          },
+          draw(progress) {
+            modal.style.width = progress * 100 + '%';
+            modalContent.style.top = (progress - 0.5) * 20 + '%';
+            modalContent.style.opacity = progress;
+            modalForm.style.top = 500 * (1 - progress) + 'px';
+          },
+        });
     })
   );
+
+  modalForm.style.position = 'relative';
 };
 
 export default modal;
