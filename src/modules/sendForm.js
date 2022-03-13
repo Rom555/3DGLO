d2cac8b0a8cc99b28d817e7c465635aa00ff4f8c
Rom555 <romanzubrilin@gmail.com>
2022-03-13 16:06:00 +0300

    add preloader svg

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -1,7 +1,34 @@
 const sendForm = ({ formId, someElem = [] }) => {
   const form = document.getElementById(formId);
   const statusBlock = document.createElement('div');
-  const loadText = 'Отправка...';
+  const loadContent = `
+  <svg version="1.1" id="L4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+  viewBox="0 0 60 60" enable-background="new 0 0 0 0" xml:space="preserve" width="60" height="30">
+    <circle fill="#fff" stroke="none" cx="6" cy="50" r="6">
+      <animate
+        attributeName="opacity"
+        dur="1s"
+        values="0;1;0"
+        repeatCount="indefinite"
+        begin="0.1"/>    
+    </circle>
+    <circle fill="#fff" stroke="none" cx="26" cy="50" r="6">
+      <animate
+        attributeName="opacity"
+        dur="1s"
+        values="0;1;0"
+        repeatCount="indefinite" 
+        begin="0.2"/>       
+    </circle>
+    <circle fill="#fff" stroke="none" cx="46" cy="50" r="6">
+      <animate
+        attributeName="opacity"
+        dur="1s"
+        values="0;1;0"
+        repeatCount="indefinite" 
+        begin="0.3"/>     
+    </circle>
+  </svg>`;
   const errorText = 'Ошибка!';
   const validateErrorText = 'Неправильно заполнены поля!';
   const successText = 'Спасибо! Наш менеджер с Вами свяжется.';
@@ -54,7 +81,7 @@ const sendForm = ({ formId, someElem = [] }) => {
       return;
     }
 
-    statusBlock.textContent = loadText;
+    statusBlock.innerHTML = loadContent;
 
     formData.forEach((val, key) => {
       formBody[key] = val;
